9ac0cb23eed4b8ce16fe4562fb40f2dc58577655
Robert <rgriffin@ihmc.us>
2021-01-12 11:17:34 -0600

    Created a foot trajectory predictor and started putting it in the balance manager and am calculator

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/dynamicPlanning/comPlanning/CoMTrajectoryProvider.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/dynamicPlanning/comPlanning/CoMTrajectoryProvider.java
@@ -1,12 +1,15 @@
 package us.ihmc.commonWalkingControlModules.dynamicPlanning.comPlanning;
 
 import us.ihmc.robotics.math.trajectories.Trajectory3D;
+import us.ihmc.robotics.math.trajectories.Trajectory3DReadOnly;
 
 import java.util.List;
 
 public interface CoMTrajectoryProvider extends CoMTrajectoryPlannerInterface
 {
    List<Trajectory3D> getVRPTrajectories();
+
+   List<? extends Trajectory3DReadOnly> getCoMTrajectories();
 
    @Override
    default int getSegmentNumber(double time)
